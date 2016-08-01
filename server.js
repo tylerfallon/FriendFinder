44b42b30f718ec9088d5bd9b0e079d036964e70d
Tyler Fallon <tylerfallon@Tylers-MacBook-Pro.local>
2016-07-31 21:15:03 -0400

    updated server.js

--- server.js
+++ server.js
@@ -1,4 +1,18 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
+var app = express();
 
+var port = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type:'application/vnd.api+json'}));
+
+require('./app/routing/api-routes.js')(app); 
+require('./app/routing/html-routes.js')(app);
+
+app.listen(port, function() {
+	console.log('Listening on port ' + PORT);
+});
