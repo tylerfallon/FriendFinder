c90d0b4addd6de5af35158dac6d580cbe4491e16
Tyler Fallon <tylerfallon@Tylers-MacBook-Pro.local>
2016-07-31 22:26:27 -0400

    added friends to friends.js

---
+++ app/data/friends.js
@@ -0,0 +1,19 @@
+var friends = [{
+  name: 'Tyler',
+  image: '',
+  scores: [5, 2, 3, 1, 4, 2, 2, 1, 4, 5]
+},
+{
+  name: 'Brandon',
+  image: '',
+  scores: [5, 2, 2, 3, 4, 4, 3, 1, 3, 1]
+
+},
+{
+  name: 'Brittany',
+  image: '',
+  scores: [5, 1, 2, 1, 3, 2, 5, 2, 4, 2]
+},
+];
+
+module.exports = friends;
